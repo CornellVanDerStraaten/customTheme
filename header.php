a57e9010a5fdf09370f79b965512944d3cb306fd
Cornell van der Straaten <30183@ma-web.nl>
2020-06-19 11:58:30 +0200

    Filmpjes tot en met 6

--- header.php
+++ header.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="<?php bloginfo('author') ?>">
+    <meta name="author" content="Cornell van der Straaten">
     <title><?php bloginfo('name') ?></title>
+    <?php wp_head(); ?>
 </head>
 <body>
+    <nav>
+        <?php wp_nav_menu(
+            array(
+                'theme_location' => 'hoofd-menu'
+            )
+            ); ?>
+    </nav>
     
-<h1><?php bloginfo('name') ?></h1>
+<h1><a href="<?php echo home_url() ?>"><?php bloginfo('name') ?></a></h1>
