c542db010c10002fd1b4872d008b87d0837cad8f
Cornell van der Straaten <30183@ma-web.nl>
2020-06-19 11:21:14 +0200

    Filmpjes t/m 3

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<?php get_header(); ?>
 <?php 
 if ( have_posts() ) :
     while( have_posts()) : the_post(): ?>
     <h3><?php the_title() ?></h3>
-    <div><?php the_title() ?></div>
+    <div><?php the_content() ?></div>
 
     <?php endwhile;
     else: ?>
